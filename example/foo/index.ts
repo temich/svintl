6278a37aae5a8dfe533f97e0e95ffcfc322e867b
temich <tema.gurtovoy@gmail.com>
2025-09-29 14:43:07 +0400

    fix: rename languages into locales in client templates

--- example/foo/index.ts
+++ example/foo/index.ts
@@ -1,11 +1,11 @@
 import { derived, writable } from 'svelte/store'
 import { create } from 'svintl'
-import { dictionaries, languages } from './built.js'
+import { dictionaries, locales } from './built.js'
 import type { Language, Dictionary } from './types'
 
-const language = writable<Language>('en-US')
-const intl = create(dictionaries, language) as Dictionary
-const dict = derived(language, ($language) => dictionaries[$language])
+const locale = writable<Language>('en-US')
+const intl = create(dictionaries, locale) as Dictionary
+const dict = derived(locale, ($locale) => dictionaries[$locale])
 
-export { dict, intl, dictionaries, languages, language }
+export { dict, intl, dictionaries, locales, locale }
 export type { Language }
